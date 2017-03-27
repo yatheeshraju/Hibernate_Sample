4115ebbec012582b3ef6ea19643954af894eba75
yatheeshraju <yatheesh.konduru@gmail.com>
2017-03-28 01:27:56 +0530

    second

--- Hibernate_Test/src/com/servlet/SetEmpData.java
+++ Hibernate_Test/src/com/servlet/SetEmpData.java
@@ -1,6 +1,7 @@
 package com.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import com.dao.HibernateDAO;
 import com.pojo.Employee;
